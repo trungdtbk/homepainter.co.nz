f34fd33c31566c6bf9579ceb0ed1889b30a73cf6
Trung Truong <trung.truong@nokia.com>
2022-08-01 17:24:32 +1200

    add navbar

---
+++ src/app/menubar/menubar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {MenuItem} from 'primeng/api';
+
+@Component({
+  selector: 'app-menubar',
+  templateUrl: './menubar.component.html',
+  styleUrls: ['./menubar.component.scss']
+})
+export class MenubarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
